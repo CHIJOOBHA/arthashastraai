d13825fc61afdbbea28844d68fe3edab7080012f
JYOTHI BHASKAR RAO CHITTI <chitti.bhargav3@gmail.com>
2026-06-09 14:25:10 +0530

    Delete check_font_classes.js

--- check_font_classes.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-const app = fs.readFileSync('src/App.tsx', 'utf8');
-const fontMatches = app.match(/font-[a-z-]+/g);
-const uniqueFonts = [...new Set(fontMatches)];
-console.log('Fonts in App.tsx:', uniqueFonts);
